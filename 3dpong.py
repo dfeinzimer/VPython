16553b579987b2b351b9e604fd099c26777f881b
David Feinzimer <dfeinzimer@gmail.com>
2018-11-09 11:11:54 -0800

    Additions

--- 3dpong.py
+++ 3dpong.py
@@ -10,10 +10,10 @@ side = 10
 center = vector(0, 0, 0)
 floor = box(pos=vector(center.x,center.y - side/2,center.z), length=side, height=thickness, width=side, color=color.blue, opacity=0.3)
 ceiling = box(pos=vector(center.x,center.y + side/2,center.z), length=side, height=thickness, width=side, color=color.blue, opacity=0.3)
-wall_l = box(pos=vector(center.x-side/2,center.y,center.z), length=thickness, height=side, width=side, color=color.red, opacity=0.3)
-wall_r = box(pos=vector(center.x+side/2,center.y,center.z), length=thickness, height=side, width=side, color=color.red, opacity=0.3)
-wall_f = box(pos=vector(center.x,center.y,center.z+side/2), length=side, height=side, width=thickness, color=color.white, opacity=0.3)
-wall_b = box(pos=vector(center.x,center.y,center.z-side/2), length=side, height=side, width=thickness, color=color.white, opacity=0.3)
+wall_1 = box(pos=vector(center.x-side/2,center.y,center.z), length=thickness, height=side, width=side, color=color.red, opacity=0.3)
+wall_2 = box(pos=vector(center.x+side/2,center.y,center.z), length=thickness, height=side, width=side, color=color.red, opacity=0.3)
+wall_3 = box(pos=vector(center.x,center.y,center.z+side/2), length=side, height=side, width=thickness, color=color.white, opacity=0.3)
+wall_4 = box(pos=vector(center.x,center.y,center.z-side/2), length=side, height=side, width=thickness, color=color.white, opacity=0.3)
 
 ball = sphere(pos=vector(0,1,0), color=color.red, radius=0.5)
 ball.velocity=vector(2,-5,-3)
@@ -21,7 +21,6 @@ ball.velocity=vector(2,-5,-3)
 ihat = vector(1, 0, 0)
 jhat = vector(0, 1, 0)
 khat = vector(0, 0, 1)
-
 
 def over_floor(ball, floor):
     fpos = floor.pos
@@ -48,7 +47,6 @@ while 1:
 #        arrow(pos=ball.pos, color=color.green, axis=ball.velocity.y*jhat, shaftwidth=0.1)
 #        arrow(pos=ball.pos, color=color.green, axis=ball.velocity.z*khat, shaftwidth=0.1)
         
-
     counter += 1
     if scene.mouse.clicked:
         scene.mouse.getclick()
